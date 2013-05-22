18319267cfcdc3df04ed57717b029eb5b636e62f
jiangsuyong <873813163@qq.com>
2013-05-22 21:17:55 +0800

    修改ListFreeClassroomAction类名

---
+++ src/main/java/com/crazyteam/action/ListFreeClassroomAction.java
@@ -0,0 +1,25 @@
+package com.crazyteam.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 
+ * @since 2013/5/22
+ *
+ */
+public class ListFreeClassroomAction extends ActionSupport {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2802000073198318674L;
+	
+	public String listFreeClassroom() throws Exception
+	{
+		return null;
+	}
+	public String listFreeClassroomByBuildNum() throws Exception
+	{
+		return null;
+	}
+}
