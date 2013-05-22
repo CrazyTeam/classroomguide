6d87ee617c635fcea85a80a3b00a545d1295e8e5
jiangsuyong <873813163@qq.com>
2013-05-22 20:25:02 +0800

    添加ListClassroomFreeAction类，这次很少，主要怕有冲突

---
+++ src/main/java/com/crazyteam/action/ListClassroomFreeAction.java
@@ -0,0 +1,12 @@
+package com.crazyteam.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 
+ * @since 2013/5/22
+ *
+ */
+public class ListClassroomFreeAction extends ActionSupport {
+	
+}
