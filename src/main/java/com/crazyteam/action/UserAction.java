6490904f6fb6f79b5d897561039ec83906ae18b0
linving <1013598664@qq.com>
2013-06-02 13:36:43 +0800

    各个层示例

--- src/main/java/com/crazyteam/action/UserAction.java
+++ src/main/java/com/crazyteam/action/UserAction.java
@@ -1,5 +1,12 @@
 package com.crazyteam.action;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import com.crazyteam.entity.User;
+import com.crazyteam.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
 /**
  * 
@@ -7,18 +14,25 @@ import com.opensymphony.xwork2.ActionSupport;
  * @author jiangsuyong
  *
  */
+@Component(value="userAction")
 public class UserAction extends ActionSupport {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7652872498317019704L;
-	
+
 	/**
 	 * 登陆
 	 * @return
 	 * @throws Exception
 	 */
+	
+	private UserService userService;
+	
+	public UserService getUserService() {
+		return userService;
+	}
+	@Resource(name="userService")
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
 	public String login() throws Exception
 	{
 		return LOGIN;
@@ -39,7 +53,14 @@ public class UserAction extends ActionSupport {
 	 */
 	public String register() throws Exception
 	{
-		return null;
+		User user = new User();
+		user.setEmail("1013598664@qq.com");
+		user.setForbid(false);
+		user.setName("linving");
+		user.setPassword("a1008611");
+		user.setWeight(1);
+		userService.saveUser(user);
+		return SUCCESS;
 	}
 
 }
